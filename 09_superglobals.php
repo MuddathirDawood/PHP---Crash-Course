37a6b5f74db7e251a27b20503819d60f6d22662a
MuddathirDawood <dmuddathir@gmail.com>
2022-12-02 11:19:19 +0200

    superglobals

---
+++ 09_superglobals.php
@@ -0,0 +1,13 @@
+<?php
+
+    /* --------------- SUPERGLOBALS --------------- */ 
+    // Built in vars that are always available in all scopes
+
+    // $_GET -      Contains info about vars passed through a URL/FORM
+    // $_POST -     Contains info about vars passed through a FORM
+    // $_COOKIE -   Contains info about vars passed through a cookie
+    // &_SESSION -  Contains info about vars passed through a session
+    // &_SERVER     Contains info about the server environment
+    // $_ENV        Contains info about the environment vars
+    // $_FILES      Contains info about files uploaded to the script
+    // $_REQUEST    Contains info about vars passed through the FROM/URL
